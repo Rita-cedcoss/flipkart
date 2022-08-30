00a300bae0a3b4930ff5704dbf6c5414e034d7f5
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-30 16:57:00 +0530

    Component 2 completed of flipkart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Component1 from './flipkart/Component1';
 import './flipkart/component.css'
+import Component2 from './flipkart/Component2';
 function App() {
   return (
      <>
      <div id="outer">
      <Component1></Component1>
+     <Component2></Component2>
      </div>
      </>
   );
--- src/flipkart/Component1.js
+++ src/flipkart/Component1.js
@@ -7,17 +7,15 @@ class Component1 extends Component {
             <img id="imgFlipkart" src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png" alt=""></img>
             <p><a href='{#}'>Flipkart Explore</a><img id="icofl" src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/plus_aef861.png" alt=""></img></p>
          </div>
-         <div id="inpComp"><input id="ipnsearch" placeholder='Search for Product, brand,or more ' type={"text"}></input><i class="fa fa-search" ></i></div>
+         <div id="inpComp"><input id="ipnsearch" placeholder='Search for Product, brand,or more ' type={"text"}></input><i class="fa fa-search" style={{color:"#2874f0"}}></i></div>
          <div id="btnlogin">Login</div>
          <div id="navabar">
             <p>Become a Seller</p>
             <select id="selid"><option>More</option></select>
             <p><i class="fa fa-shopping-cart"></i>Cart</p>
-
          </div>
       </div>
     )
   }
 }
-
 export default Component1
