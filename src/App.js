5a8714104b4073a14c07967bfd40280a6128b032
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-31 11:03:31 +0530

    Component task5 completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Component1 from './flipkart/Component1';
 import './flipkart/component.css'
 import Component2 from './flipkart/Component2';
 import Component3 from './flipkart/Component3';
+import Component4 from './flipkart/Component4';
 function App() {
   return (
      <>
@@ -10,6 +11,7 @@ function App() {
      <Component1></Component1>
      <Component2></Component2>
      <Component3></Component3>
+     <Component4></Component4>
      </div>
      </>
   );
---
+++ src/flipkart/Component4.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+
+class Component4 extends Component {
+  render() {
+    return (
+      <div id="Component4">
+        <div id="Comp4left">
+            <div id="Comp4divTop">
+                <div>Deals of the day</div>
+                <div><button id="btnView">View All</button></div>
+            </div>
+            <div id="Comp4divBottom">
+                <div class="Product_item">
+                    <div><img  src="https://images.livspace-cdn.com/plain/https://jumanji.livspace-cdn.com/magazine/wp-content/uploads/sites/2/2019/10/12170744/cover-2-3-1.jpg" alt=''></img></div>
+                    <div>
+                      <p>Deals on Furnishing</p>
+                      <p>upto 80% off</p>
+                    
+                    </div>
+                </div>
+                <div class="Product_item">
+                    <div><img  src="https://rukminim1.flixcart.com/image/612/612/kt7jv680/air-fryer/k/e/g/black-4-1-1400-hd9252-70-philips-original-imag6hx3nxvsjksv.jpeg?q=70" alt=''></img></div>
+                    <div>
+                      <p>Air fryer</p>
+                      <p>upto 550% off</p>
+                    </div>
+                </div>
+                <div class="Product_item">
+                    <div><img  src="https://cdn3.volusion.com/upehv.auyze/v/vspfiles/photos/MCCH1-2.jpg?v-cache=1354259150" alt=''></img></div>
+                    <div>
+                      <p>Best deals with kids clothes</p>
+                      <p>upto 60% off</p>
+                    </div>
+                </div>
+                <div class="Product_item">
+                    <div><img  src="https://m.media-amazon.com/images/I/71uFE2PrjlL.jpg" alt=''></img></div>
+                    <div>
+                      <p>Remote Control Toys</p>
+                      <p>upto 80% off</p>
+                    </div>
+                </div>
+                <div class="Product_item">
+                    <div><img  src="https://stylesatlife.com/wp-content/uploads/2019/05/Black-Bed-Sheet-Designs.jpg.webp" alt=''></img></div>
+                    <div>
+                      <p>Designer Bed sheets</p>
+                      <p>From ₹159 </p>
+                    </div>
+                </div>
+                <div class="Product_item">
+                    <div><img  src="https://femina.wwmindia.com/content/2021/nov/saree51635751801.jpg" alt=''></img></div>
+                    <div>
+                      <p>Sarees for Wedding</p>
+                      <p>From ₹199 + Extra 10% off</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="Comp4right">
+            <img src="https://rukminim1.flixcart.com/fk-p-flap/464/708/image/e5d43b308280f531.jpg?q=70" style={{height:"100%", width:"100%",boxShadow:"0px 0px 0px 1px black"}} alt=""></img>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Component4
